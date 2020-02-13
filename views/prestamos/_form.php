75ed658c257266a9b77cbf45c9e4df6324678ae3
Ricardo Pérez <ricpelo@gmail.com>
2020-02-13 19:36:53 +0100

    Otro

--- views/prestamos/_form.php
+++ views/prestamos/_form.php
@@ -2,10 +2,18 @@
 
 use yii\bootstrap4\Html;
 use yii\bootstrap4\ActiveForm;
+use yii\web\View;
 
 /* @var $this yii\web\View */
 /* @var $model app\models\Prestamos */
 /* @var $form yii\bootstrap4\ActiveForm */
+$js = <<<EOT
+    $('#prestamos-libro_id').on('change', function (ev) {
+        var el = $(this);
+        $('#libro_id').text(el.val());
+    });
+EOT;
+$this->registerJs($js);
 ?>
 
 <div class="prestamos-form">
@@ -14,6 +22,7 @@ use yii\bootstrap4\ActiveForm;
         'id' => 'create-prestamos-form',
         'enableAjaxValidation' => true,
     ]); ?>
+        <span id="libro_id"></span>
         <?= $form->field($model, 'libro_id')->label('Libro')->dropDownList($libros) ?>
         <?= $form->field($model, 'lector_id')->label('Lector')->dropDownList($lectores) ?>
 
